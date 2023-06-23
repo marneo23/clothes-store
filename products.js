701c2bafbc1b53e7c75133555c1b05db8970b042
marodriguez@quadiontech.com <marodriguez@quadiontech.com>
2023-06-23 15:10:42 -0300

    [UPDATE

---
+++ products.js
@@ -0,0 +1,38 @@
+products[
+  ({
+    id: 1,
+    value: 5,
+    name: "'ESSENTIALS' CAP",
+    img: "assets/img/catalog/cap1.jpg",
+  },
+  {
+    id: 2,
+    value: 20,
+    name: "MOCHA PARKA",
+    img: "assets/img/catalog/parka1.jpg",
+  },
+  {
+    id: 3,
+    value: 10,
+    name: "NATURE TEE",
+    img: "assets/img/catalog/tshirt1.jpg",
+  },
+  {
+    id: 4,
+    value: 5,
+    name: "'ESSENTIALS' BUCKET CAP",
+    img: "assets/img/catalog/cap2.jpg",
+  },
+  {
+    id: 5,
+    value: 22,
+    name: "NIGHT PARKA",
+    img: "assets/img/catalog/parka2.jpg",
+  },
+  {
+    id: 6,
+    value: 7,
+    name: "'ESSENTIALS' BAG",
+    img: "assets/img/catalog/tshirt1.jpg",
+  })
+];
